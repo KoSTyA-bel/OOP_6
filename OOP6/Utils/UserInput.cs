75dbc536ea03dcf4c633b704ddefc6d5d36ddf73
KoSTyA-bel <kostyafedorakin@gmail.com>
2020-12-05 17:53:08 +0300

    Initial commit

---
+++ OOP6/Utils/UserInput.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP4.Utils
+{
+    static class UserInput
+    {
+        public static string GetString()
+        {
+            return Console.ReadLine();
+        }
+        public static int GetInt()
+        {
+            while (true)
+            {
+                try
+                {
+                    return Convert.ToInt32(GetString());
+                }
+                catch
+                {
+                    Console.WriteLine("Input correct data");
+                }
+            }
+        }
+    }
+}
---
+++ OOP6/View/OutInConsole.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP4.View
+{
+    static class OutInConsole
+    {
+        public static void Print<T>(T message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
