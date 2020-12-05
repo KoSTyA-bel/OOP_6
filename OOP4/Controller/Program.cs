f76ebeebf26b2299dd72a35341b76a65b56463fd
KoSTyA-bel <kostyafedorakin@gmail.com>
2020-12-06 00:00:14 +0300

    Full worked

---
+++ OOP4/Controller/Program.cs
@@ -0,0 +1,20 @@
+﻿using OOP4.View;
+using OOP4.Utils;
+using OOP4.Models;
+
+namespace OOP4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int size = UserInput.GetInt();
+            int[] arr = new int[size];
+            for (int i = 0; i < size; i++)
+            {
+                arr[i] = UserInput.GetInt();
+            }
+            OutInConsole.Print(Logic.SymOfDigitsAfterLastZero(arr));
+        }
+    }
+}
---
+++ OOP4/Models/Logic.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP4.Models
+{
+    static class Logic
+    {
+        public static string SymOfDigitsAfterLastZero(int[] array)
+        {
+            int maxIndexZero = -1;
+            for (int i = 0; i < array.Length; i++)
+            {
+                if (array[i] == 0)
+                {
+                    maxIndexZero = i;
+                }
+            }
+            if (maxIndexZero != -1 && maxIndexZero + 1 != array.Length)
+            {
+                int sum = 0;
+                for (int i = maxIndexZero; i < array.Length; i++)
+                {
+                    sum += array[i];
+                }
+                return Convert.ToString(sum);
+            }
+            return "No sum";
+        }
+    }
+}
