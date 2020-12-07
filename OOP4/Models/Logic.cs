b3030110780af537f6a0fb150c19ef7c65f46f7b
KoSTyA-bel <kostyafedorakin@gmail.com>
2020-12-07 19:41:29 +0300

    second part

--- OOP4/Models/Logic.cs
+++ OOP4/Models/Logic.cs
@@ -10,66 +10,17 @@ namespace OOP6.Models
             return Convert.ToString(array.Skip(Array.LastIndexOf(array, 0)).Sum());
         }
 
-        public static void swap(ref int x, ref int y)
-        { 
-            x = x ^ y;
-            y = y ^ x;
-            x = x ^ y;
-        }
-
-
-        private static int GetGreatestNumberIndex(int[] arr)
-        {
-            int imax = 0;
-            for(int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] > arr[imax])
-                {
-                    imax = i;
-                }
-            }
-            return imax;
-        }
-
-        private static int GetLowestNumberIndex(int[] arr)
-        {
-            int imin = 0;
-            for(int i = 0; i < arr.Length; i++)
-            {
-                if (arr[i] < arr[imin])
-                {
-                    imin = i;
-                }
-            }
-            return imin;
-        }
-
-
         public static int GetSumSinceGreatestTillLowest(int[] arr)
         {
-            if (arr.Length < 1)
+            if (Array.LastIndexOf(arr, arr.Max()) < Array.LastIndexOf(arr, arr.Min()))
             {
-                return -1;
+                return arr.Skip(Array.LastIndexOf(arr, arr.Min()))
+                    .Take(Array.LastIndexOf(arr, arr.Min()) - Array.LastIndexOf(arr, arr.Max()))
+                    .Sum();
             }
-            else if(arr.Length == 1)
-            {
-                return arr[0];
-            }
-            int sum = 0;
-            int imax = GetGreatestNumberIndex(arr);
-            int imin = GetLowestNumberIndex(arr);
-            if (imin > imax)
-            {
-                swap(ref imin, ref imax);
-            }
-            for(int i = imin; i <= imax; i++)
-            {
-                sum += arr[i];
-            }
-            return sum;
+            return arr.Skip(Array.LastIndexOf(arr, arr.Max()))
+                .Take(Array.LastIndexOf(arr, arr.Max()) - Array.LastIndexOf(arr, arr.Min()))
+                .Sum();
         }
-
-
-
     }
 }
