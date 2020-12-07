44dddf72a1e7c2cff65ece93d1e9eb95b72ec10c
KoSTyA-bel <kostyafedorakin@gmail.com>
2020-12-07 19:15:07 +0300

    changed stupid code

--- OOP4/Models/Logic.cs
+++ OOP4/Models/Logic.cs
@@ -1,6 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
 
 namespace OOP6.Models
 {
@@ -8,24 +7,7 @@ namespace OOP6.Models
     {
         public static string SymOfDigitsAfterLastZero(int[] array)
         {
-            int maxIndexZero = -1;
-            for (int i = 0; i < array.Length; i++)
-            {
-                if (array[i] == 0)
-                {
-                    maxIndexZero = i;
-                }
-            }
-            if (maxIndexZero != -1 && maxIndexZero + 1 != array.Length)
-            {
-                int sum = 0;
-                for (int i = maxIndexZero; i < array.Length; i++)
-                {
-                    sum += array[i];
-                }
-                return Convert.ToString(sum);
-            }
-            return "No sum";
+            return Convert.ToString(array.Skip(Array.LastIndexOf(array, 0)).Sum());
         }
 
         public static void swap(ref int x, ref int y)
